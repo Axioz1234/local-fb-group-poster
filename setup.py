40541da1c9e45537967834fd1ffe3a8e9251a90d
Axioz1234 <lukedevries10@gmail.com>
2025-02-27 17:03:10 +0930

    setup

--- setup.py
+++ setup.py
@@ -4,4 +4,13 @@ setup(
     name="local-fb-group-poster",
     version="0.1",
     packages=["facebook_poster"],
+    install_requires=[
+        line.strip()
+        for line in open("requirements.txt").readlines()
+    ],
+    entry_points={
+        'console_scripts': [
+            'local-fb-group-poster=facebook_poster.main:FacebookPoster().run',
+        ],
+    },
 )
