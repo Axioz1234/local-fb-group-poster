a1d66a2df20dabe5289665a40b5114be5ac23e0c
Axioz1234 <lukedevries10@gmail.com>
2025-02-27 13:31:08 +0930

    Commit to main
    commented out headless

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ class FacebookPoster:
         try:
             chrome_options = webdriver.ChromeOptions()
             chrome_options.add_argument('--no-sandbox')
-            chrome_options.add_argument('--headless')
+            #chrome_options.add_argument('--headless')
             chrome_options.add_argument('--disable-dev-shm-usage')
             chrome_options.add_argument('--disable-gpu')
             prefs = {"profile.default_content_setting_values.notifications": 2}
